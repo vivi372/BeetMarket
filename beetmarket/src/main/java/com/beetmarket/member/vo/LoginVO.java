aef99ce72bbc98c43f19d7c0bdc21910121b2bd9
vivi372 <ym970825@naver.com>
2024-09-10 13:56:57 +0900

    스프링 레거시 프로젝트 커밋

---
+++ beetmarket/src/main/java/com/beetmarket/member/vo/LoginVO.java
@@ -0,0 +1,14 @@
+package com.beetmarket.member.vo;
+
+import lombok.Data;
+
+@Data
+public class LoginVO {
+	private String id;
+	private String pw;
+	private String name;
+	private String photo;
+	private Long newMsgCnt;
+	private Integer gradeNo;
+	private String gradeName;
+}
