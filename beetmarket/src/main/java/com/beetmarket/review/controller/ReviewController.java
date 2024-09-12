920cc259558911cf1a9b5d426c8567b6b401fc9c
MoonSung <MoonSung@DESKTOP-4F52AFG.mshome.net>
2024-09-12 10:23:07 +0900

    123

--- beetmarket/src/main/java/com/beetmarket/review/controller/ReviewController.java
+++ beetmarket/src/main/java/com/beetmarket/review/controller/ReviewController.java
@@ -1,47 +1,47 @@
-package com.beetmarket.review.controller;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.beetmarket.review.service.ReviewService;
-import com.beetmarket.review.vo.ReviewVO;
-import com.webjjang.util.page.PageObject;
-
-import lombok.extern.log4j.Log4j;
-
-@RestController
-@RequestMapping("/review")
-@Log4j
-public class ReviewController {
-
-	@Autowired
-	@Qualifier("reviewServiceImpl")
-	private ReviewService service;
-	
-	//1. 리스트
-	 @GetMapping(value = "/review.do", 
-			 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	 public ResponseEntity<Map<String, Object>>
-	 	list(PageObject pageObject, Long no) {
-		 log.info("list - page : " + pageObject.getPage() + ", no : " + no);
-		 
-		 List<ReviewVO> list = service.list(pageObject, no);
-		 Map<String, Object> map = new HashMap<String, Object>();
-		 map.put("list", list);
-		 map.put("pageObject", pageObject);
-		 
-		 return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
-				 
-	 }
-	
-}
+//package com.beetmarket.review.controller;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.beetmarket.review.service.ReviewService;
+//import com.beetmarket.review.vo.ReviewVO;
+//import com.webjjang.util.page.PageObject;
+//
+//import lombok.extern.log4j.Log4j;
+//
+//@RestController
+//@RequestMapping("/review")
+//@Log4j
+//public class ReviewController {
+//
+//	@Autowired
+//	@Qualifier("reviewServiceImpl")
+//	private ReviewService service;
+//	
+//	//1. 리스트
+//	 @GetMapping(value = "/review.do", 
+//			 produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	 public ResponseEntity<Map<String, Object>>
+//	 	list(PageObject pageObject, Long no) {
+//		 log.info("list - page : " + pageObject.getPage() + ", no : " + no);
+//		 
+////		 // List<ReviewVO> list = service.list(pageObject, no);
+////		 Map<String, Object> map = new HashMap<String, Object>();
+////		 map.put("list", list);
+////		 map.put("pageObject", pageObject);
+//		 
+////		 return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+////				 
+//	 }
+//	
+//}
--- beetmarket/src/main/java/com/beetmarket/review/service/ReviewServiceImpl.java
+++ beetmarket/src/main/java/com/beetmarket/review/service/ReviewServiceImpl.java
@@ -1,30 +1,30 @@
-package com.beetmarket.review.service;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-
-import com.beetmarket.review.vo.ReviewVO;
-import com.webjjang.util.page.PageObject;
-
-import lombok.extern.log4j.Log4j;
-
-@Service
-@Log4j
-@Qualifier("ReviewServiceImpl")
-public class ReviewServiceImpl implements ReviewService {
-
-	@Inject
-	private ReviewMapper mapper;
-	
-	@Override
-	public List<ReviewVO> list(PageObject pageObject){
-		log.info("list() 실행");
-		
-		pageObject.setTotalRow(mapper.getTotalRow(pageObject));
-		return mapper.list(pageObject);
-	}
-}
+//package com.beetmarket.review.service;
+//
+//import java.util.List;
+//
+//import javax.inject.Inject;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.stereotype.Service;
+//
+//import com.beetmarket.review.vo.ReviewVO;
+//import com.webjjang.util.page.PageObject;
+//
+//import lombok.extern.log4j.Log4j;
+//
+//@Service
+//@Log4j
+//@Qualifier("ReviewServiceImpl")
+//public class ReviewServiceImpl implements ReviewService {
+//
+//	@Inject
+//	private ReviewMapper mapper;
+//	
+//	@Override
+//	public List<ReviewVO> list(PageObject pageObject){
+//		log.info("list() 실행");
+//		
+//		pageObject.setTotalRow(mapper.getTotalRow(pageObject));
+//		return mapper.list(pageObject);
+//	}
+//}
